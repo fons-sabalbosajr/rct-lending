9a05941fc2556156efdf3e2b47e9d95c8a2e1020
Fons Sabalbosa <86940332+fons-sabalbosajr@users.noreply.github.com>
2024-03-13 14:24:12 +0800

    initialize new project rct-lending

---
+++ rct-app/frm_splashscreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rct_app
+{
+    public partial class frm_splashscreen : Form
+    {
+        public frm_splashscreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
