2a1985e007a5087331ddb3b5922ee910b573fc97
Fons Sabalbosa <86940332+fons-sabalbosajr@users.noreply.github.com>
2024-06-22 17:04:07 +0800

    done uploading google drive and mongodb uploading
    test

--- rct-app/rct-lmis/rct-lmis/frm_test_upload.Designer.cs
+++ rct-app/rct-lmis/rct-lmis/frm_test_upload.Designer.cs
@@ -29,59 +29,50 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lpercent = new System.Windows.Forms.Label();
-            this.pbloading = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.btnAddFiles = new System.Windows.Forms.Button();
             this.bupload = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.rtpath = new System.Windows.Forms.RichTextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.bopen = new System.Windows.Forms.Button();
             this.taddress = new System.Windows.Forms.TextBox();
             this.tname = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.mainProgressBar = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.dgvFiles = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.lpercent);
-            this.groupBox1.Controls.Add(this.pbloading);
+            this.groupBox1.Controls.Add(this.btnAddFiles);
+            this.groupBox1.Controls.Add(this.dgvFiles);
             this.groupBox1.Controls.Add(this.bupload);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.rtpath);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.bopen);
             this.groupBox1.Controls.Add(this.taddress);
             this.groupBox1.Controls.Add(this.tname);
-            this.groupBox1.Location = new System.Drawing.Point(31, 35);
+            this.groupBox1.Location = new System.Drawing.Point(6, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(429, 266);
+            this.groupBox1.Size = new System.Drawing.Size(668, 329);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Details";
             // 
-            // lpercent
+            // btnAddFiles
             // 
-            this.lpercent.AutoSize = true;
-            this.lpercent.Location = new System.Drawing.Point(256, 236);
-            this.lpercent.Name = "lpercent";
-            this.lpercent.Size = new System.Drawing.Size(62, 13);
-            this.lpercent.TabIndex = 11;
-            this.lpercent.Text = "uploading...";
-            // 
-            // pbloading
-            // 
-            this.pbloading.Location = new System.Drawing.Point(195, 210);
-            this.pbloading.Name = "pbloading";
-            this.pbloading.Size = new System.Drawing.Size(123, 23);
-            this.pbloading.TabIndex = 10;
-            this.pbloading.Text = "guna2ProgressBar1";
-            this.pbloading.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            this.btnAddFiles.Location = new System.Drawing.Point(506, 110);
+            this.btnAddFiles.Name = "btnAddFiles";
+            this.btnAddFiles.Size = new System.Drawing.Size(75, 27);
+            this.btnAddFiles.TabIndex = 10;
+            this.btnAddFiles.Text = "Add File";
+            this.btnAddFiles.UseVisualStyleBackColor = true;
+            this.btnAddFiles.Click += new System.EventHandler(this.btnAddFiles_Click);
             // 
             // bupload
             // 
-            this.bupload.Location = new System.Drawing.Point(114, 210);
+            this.bupload.Location = new System.Drawing.Point(587, 110);
             this.bupload.Name = "bupload";
-            this.bupload.Size = new System.Drawing.Size(75, 23);
+            this.bupload.Size = new System.Drawing.Size(75, 27);
             this.bupload.TabIndex = 3;
             this.bupload.Text = "Upload File";
             this.bupload.UseVisualStyleBackColor = true;
@@ -90,41 +81,24 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 60);
+            this.label3.Location = new System.Drawing.Point(7, 102);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(48, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Address:";
             // 
-            // rtpath
-            // 
-            this.rtpath.Location = new System.Drawing.Point(33, 104);
-            this.rtpath.Name = "rtpath";
-            this.rtpath.Size = new System.Drawing.Size(285, 96);
-            this.rtpath.TabIndex = 1;
-            this.rtpath.Text = "";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 35);
+            this.label2.Location = new System.Drawing.Point(7, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 6;
             this.label2.Text = "Name:";
             // 
-            // bopen
-            // 
-            this.bopen.Location = new System.Drawing.Point(33, 210);
-            this.bopen.Name = "bopen";
-            this.bopen.Size = new System.Drawing.Size(75, 23);
-            this.bopen.TabIndex = 2;
-            this.bopen.Text = "Open File";
-            this.bopen.UseVisualStyleBackColor = true;
-            // 
             // taddress
             // 
-            this.taddress.Location = new System.Drawing.Point(82, 57);
+            this.taddress.Location = new System.Drawing.Point(59, 99);
             this.taddress.Multiline = true;
             this.taddress.Name = "taddress";
             this.taddress.Size = new System.Drawing.Size(236, 38);
@@ -132,10 +106,36 @@
             // 
             // tname
             // 
-            this.tname.Location = new System.Drawing.Point(82, 31);
+            this.tname.Location = new System.Drawing.Point(59, 73);
             this.tname.Name = "tname";
-            this.tname.Size = new System.Drawing.Size(181, 20);
+            this.tname.Size = new System.Drawing.Size(236, 20);
             this.tname.TabIndex = 4;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(11, 365);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(62, 13);
+            this.statusLabel.TabIndex = 11;
+            this.statusLabel.Text = "uploading...";
+            // 
+            // mainProgressBar
+            // 
+            this.mainProgressBar.Location = new System.Drawing.Point(12, 347);
+            this.mainProgressBar.Name = "mainProgressBar";
+            this.mainProgressBar.Size = new System.Drawing.Size(662, 14);
+            this.mainProgressBar.TabIndex = 10;
+            this.mainProgressBar.Text = "guna2ProgressBar1";
+            this.mainProgressBar.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            // 
+            // dgvFiles
+            // 
+            this.dgvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFiles.Location = new System.Drawing.Point(6, 143);
+            this.dgvFiles.Name = "dgvFiles";
+            this.dgvFiles.Size = new System.Drawing.Size(656, 159);
+            this.dgvFiles.TabIndex = 13;
             // 
             // frm_test_upload
             // 
@@ -143,13 +143,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(686, 513);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.mainProgressBar);
             this.Name = "frm_test_upload";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frm_test_upload";
             this.Load += new System.EventHandler(this.frm_test_upload_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -158,12 +162,12 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button bupload;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox rtpath;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button bopen;
         private System.Windows.Forms.TextBox taddress;
         private System.Windows.Forms.TextBox tname;
-        private Guna.UI2.WinForms.Guna2ProgressBar pbloading;
-        private System.Windows.Forms.Label lpercent;
+        private Guna.UI2.WinForms.Guna2ProgressBar mainProgressBar;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.DataGridView dgvFiles;
+        private System.Windows.Forms.Button btnAddFiles;
     }
 }
--- rct-app/rct-lmis/rct-lmis/frm_test_upload.cs
+++ rct-app/rct-lmis/rct-lmis/frm_test_upload.cs
@@ -8,36 +8,39 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
+using System.Windows.Shapes;
 
 namespace rct_lmis
 {
     public partial class frm_test_upload : Form
     {
-
         private IMongoCollection<BsonDocument> collection;
         private static string[] Scopes = { DriveService.Scope.DriveFile };
         private static string ApplicationName = "rct-lmis";
         private DriveService service;
+
+
+        // Folder IDs
+        private static string DocsFolderId = "1kMd3QjEw95oJsMSAK9xwEf-I3_MKlMBj";
+        private static string ImagesFolderId = "1O_-PLQyRAjUV7iy6d3PN5rLXznOzxean";
+
+        private List<string> filePaths = new List<string>();
 
         public frm_test_upload()
         {
             InitializeComponent();
             InitializeMongoDB();
             InitializeGoogleDrive();
+            InitializeDataGridView();
         }
 
         private void frm_test_upload_Load(object sender, EventArgs e)
         {
-
         }
 
         private void InitializeMongoDB()
@@ -46,79 +49,164 @@ namespace rct_lmis
             collection = database.GetCollection<BsonDocument>("test-data");
         }
 
-        [Obsolete]
+
         private async void InitializeGoogleDrive()
         {
-            UserCredential credential;
-            using (var stream = new FileStream("google_drive_credentials.json", FileMode.Open, FileAccess.Read))
+            try
             {
-                var clientSecrets = GoogleClientSecrets.Load(stream).Secrets;
-                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    clientSecrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore("token.json", true));
-            }
+                UserCredential credential;
+                using (var stream = new FileStream("google_drive_credentials.json", FileMode.Open, FileAccess.Read))
+                {
+                    var clientSecrets = GoogleClientSecrets.Load(stream).Secrets;
+                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        clientSecrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore("token.json", true));
+                }
 
-            // Create Drive API service
-            service = new DriveService(new BaseClientService.Initializer()
+                // Create Drive API service
+                service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+            }
+            catch (Exception ex)
             {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
+                MessageBox.Show($"Error initializing Google Drive service: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void InitializeDataGridView()
+        {
+            dgvFiles.AutoGenerateColumns = false;
+            dgvFiles.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                Name = "FileName",
+                HeaderText = "File Name",
+                DataPropertyName = "FileName",
+                Width = 300
+            });
+            dgvFiles.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                Name = "Status",
+                HeaderText = "Status",
+                DataPropertyName = "Status",
+                Width = 150
             });
         }
+
 
         private async void bupload_Click(object sender, EventArgs e)
         {
             try
             {
-                // Open file dialog to select file
-                OpenFileDialog openFileDialog = new OpenFileDialog();
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                if (filePaths.Count == 0)
                 {
-                    string filePath = openFileDialog.FileName;
+                    MessageBox.Show("Please select files to upload.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
 
-                    // Initialize progress bar and label
-                    pbloading.Value = 0;
-                    lpercent.Text = "0%";
+                mainProgressBar.Maximum = filePaths.Count;
+                mainProgressBar.Value = 0;
+
+                statusLabel.Text = "Uploading files...";
+                statusLabel.ForeColor = Color.Black;
+
+                for (int i = 0; i < filePaths.Count; i++)
+                {
+                    string filePath = filePaths[i];
+                    string name = tname.Text;
+                    string address = taddress.Text;
+
+                    if (string.IsNullOrEmpty(filePath) || string.IsNullOrEmpty(name))
+                    {
+                        MessageBox.Show("Please fill in all fields and select a file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    string mimeType = GetMimeType(filePath);
+                    string destinationFolderId = GetDestinationFolderId(mimeType);
+                    string originalFileName = System.IO.Path.GetFileName(filePath);
+                    string newFileName = $"{name} - {originalFileName}";
+
+                    // Update status in DataGridView (Invoke required for cross-thread access)
+                    dgvFiles.Invoke((MethodInvoker)(() =>
+                    {
+                        if (i < dgvFiles.Rows.Count)
+                            dgvFiles.Rows[i].Cells["Status"].Value = "Uploading";
+                    }));
 
                     // Upload file to Google Drive
-                    string fileId = await UploadFileToDrive(filePath);
+                    string fileId = await UploadFileToDrive(filePath, newFileName, destinationFolderId);
 
-                    // Save data to MongoDB
-                    var document = new BsonDocument
+                    // Update status in DataGridView (Invoke required for cross-thread access)
+                    dgvFiles.Invoke((MethodInvoker)(() =>
                     {
-                        { "Name", tname.Text },
-                        { "Address", taddress.Text },
-                        { "FilePath", fileId }
-                    };
+                        if (i < dgvFiles.Rows.Count)
+                        {
+                            if (fileId != null)
+                                dgvFiles.Rows[i].Cells["Status"].Value = "Upload Done";
+                            else
+                                dgvFiles.Rows[i].Cells["Status"].Value = "Failed";
+                        }
+                    }));
 
-                    await collection.InsertOneAsync(document);
+                    // Update progress bar (Invoke required for cross-thread access)
+                    mainProgressBar.Invoke((MethodInvoker)(() =>
+                    {
+                        mainProgressBar.Value = i + 1;
+                    }));
 
-                    MessageBox.Show("Data and file uploaded successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    // Save data to MongoDB for successful uploads
+                    if (fileId != null)
+                    {
+                        // Generate Google Drive link
+                        string fileLink = $"https://drive.google.com/file/d/{fileId}/view?usp=sharing";
+
+                        // Save data to MongoDB
+                        var document = new BsonDocument
+                {
+                    { "Name", name },
+                    { "Address", address },
+                    { "FilePath", fileLink },
+                };
+
+                        await collection.InsertOneAsync(document);
+                    }
                 }
+
+                // Clear form fields after successful upload
+                tname.Invoke((MethodInvoker)(() => tname.Text = ""));
+                taddress.Invoke((MethodInvoker)(() => taddress.Text = ""));
+                filePaths.Clear();
+                dgvFiles.Invoke((MethodInvoker)(() => dgvFiles.Rows.Clear()));
+
+                // Update status label
+                statusLabel.Text = "Upload completed!";
+                statusLabel.ForeColor = Color.Green;
+
+                MessageBox.Show("Data and files uploaded successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error uploading file and saving data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            finally
-            {
-                // Reset progress bar and label
-                pbloading.Value = 0;
-                lpercent.Text = "0%";
+                MessageBox.Show($"Error uploading files and saving data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        private async Task<string> UploadFileToDrive(string filePath)
+
+
+        private async Task<string> UploadFileToDrive(string filePath, string newFileName, string folderId)
         {
             try
             {
                 var fileMetadata = new Google.Apis.Drive.v3.Data.File()
                 {
-                    Name = Path.GetFileName(filePath),
-                    Parents = new List<string> { "1kMd3QjEw95oJsMSAK9xwEf-I3_MKlMBj" } // Folder ID
+                    Name = newFileName,
+                    Parents = new List<string> { folderId }
                 };
 
                 FilesResource.CreateMediaUpload request;
@@ -128,27 +216,10 @@ namespace rct_lmis
                     request = service.Files.Create(fileMetadata, stream, GetMimeType(filePath));
                     request.Fields = "id";
 
-                    request.ProgressChanged += (IUploadProgress progress) =>
-                    {
-                        switch (progress.Status)
-                        {
-                            case UploadStatus.Uploading:
-                                int progressPercentage = (int)((progress.BytesSent * 100) / stream.Length);
-                                UpdateProgressBar(progressPercentage);
-                                break;
-                            case UploadStatus.Completed:
-                                UpdateProgressBar(100);
-                                break;
-                            case UploadStatus.Failed:
-                                MessageBox.Show($"Error uploading file to Google Drive: {progress.Exception.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                                break;
-                        }
-                    };
-
                     await request.UploadAsync();
                 }
 
-                return request.ResponseBody.Id;
+                return request.ResponseBody?.Id;
             }
             catch (Exception ex)
             {
@@ -157,25 +228,44 @@ namespace rct_lmis
             }
         }
 
-        private void UpdateProgressBar(int progress)
-        {
-            BeginInvoke(new Action(() =>
-            {
-                pbloading.Value = progress;
-                lpercent.Text = $"{progress}%";
-            }));
-        }
 
         private string GetMimeType(string fileName)
         {
             string mimeType = "application/unknown";
-            string ext = Path.GetExtension(fileName).ToLower();
+            string ext = System.IO.Path.GetExtension(fileName).ToLower();
             Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
             if (regKey != null && regKey.GetValue("Content Type") != null)
             {
                 mimeType = regKey.GetValue("Content Type").ToString();
             }
             return mimeType;
+        }
+
+        private string GetDestinationFolderId(string mimeType)
+        {
+            if (mimeType.StartsWith("image/"))
+            {
+                return ImagesFolderId;
+            }
+            else
+            {
+                return DocsFolderId;
+            }
+        }
+
+        private void btnAddFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string fileName in openFileDialog.FileNames)
+                {
+                    filePaths.Add(fileName);
+                    dgvFiles.Rows.Add(System.IO.Path.GetFileName(fileName), "Pending");
+                }
+            }
         }
     }
 }
